f455f77be35848d818aad90ed3c494afd7d92fef
Amndeep Singh Mann <me@asm.works>
2024-12-26 05:18:47 +0000

    get rid of deprecation warning by manually creating the new event loop

--- mass_mentioner/mass_mentioner.py
+++ mass_mentioner/mass_mentioner.py
@@ -59,5 +59,6 @@ async def main():
                 parent = await parent.reply(message)
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     loop.run_until_complete(main())
